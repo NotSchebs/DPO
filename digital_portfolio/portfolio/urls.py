6e076c73f5d7791376154813dc2fcba2098b4877
NotSchebs <123877562+NotSchebs@users.noreply.github.com>
2025-05-25 17:42:18 +0200

    a

--- digital_portfolio/portfolio/urls.py
+++ digital_portfolio/portfolio/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('blog/<int:post_id>/', views.post_detail, name='post_detail'),
     path('contact/', views.contact, name='contact'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
